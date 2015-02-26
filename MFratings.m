49d15a63c2c49b63f063618c339a1b62c3d93c2e
Brian Doolittle <10bddoolittle@gmail.com>
2015-02-25 21:20:01 -0500

    MFratings complete

--- MFratings.m
+++ MFratings.m
@@ -1,5 +1,25 @@
-function M = MFratings()
-% 
+function M = MFratings(Xtrain)
+% Creates Base matrix of users and tracks
 
+% INPUTS:
+% Xtrain     Data set of users tracks and ratings
+
+% M          Matrix of [users,tracks]
+
+[m,n] = size(Xtrain);
+mxU = max(Xtrain(:,3));
+mxT = max(Xtrain(:,2));
+
+% initializing Matrix
+M = zeros(mxU+1,mxT+1);
+
+for i = 1:m
+    
+    uidx = Xtrain(i,3) + 1;
+    tidx = Xtrain(i,2) + 1;
+    M(uidx,tidx) = Xtrain(i,4);
+    
+end
 
 end
+
