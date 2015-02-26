8747c81a7d1ffbf2979e9c0bbd306f7c17dc87e4
Pratap <luitelpratap@gmail.com>
2015-02-25 23:17:44 -0500

    MFTrain with updated gradient descent

--- MFtrain.m
+++ MFtrain.m
@@ -7,29 +7,38 @@ function theta = MFtrain(M,U)
 
 % Output
 %theta :[1 x k]
-
-lambda = 1;
-gamma = -1;
+tic;
+lambda = 0.001;
+gamma = -0.001;
 
 [nUsers,nTracks] = size(M); 
 [~,nFeatures] = size(U);
 %initialize theta randomly
 thetaInit = rand(nFeatures,1);
 %assign the initialized theta
-theta = thetaInit;
+theta = zeros(nFeatures,nTracks);
 
+iter = 0;
 
 %gradient descent implementation
-for iterUser = 1:nUsers
-    for iterTrack = 1:nTracks
-        
+for iterTrack = 1:nTracks
+    theta(:,iterTrack) = thetaInit;
+    while (iter < 5)
+    for iterUser = 1:nUsers
         actualRating = M(iterUser,iterTrack);
-        userProfile = U(iterUser,:);
-        predictedRating = userProfile*thetaInit;
-        
-        diff = actualRating - predictedRating;
-        theta = theta + gamma*(diff*userProfile' - lambda*theta);
+        %for each track a user has rated, 
+        if (actualRating > 0)
+            userProfile = U(iterUser,:);
+            predictedRating = userProfile*theta(:,iterTrack);
+            diff = actualRating - predictedRating;
+            %update theta
+            theta(:,iterTrack) = theta(:,iterTrack)...
+                                + gamma*(diff*userProfile' - lambda*theta(:,iterTrack));      
+        end
     end
+    iter =  iter + 1; 
+    end
+    
 end
-
+toc;
 end
